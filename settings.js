c87bf8c527e582ff0de10691b7e138129345994b
gabe-luamaster <cynthiauko@yahoo.com>
2026-02-13 15:16:14 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,40 +1,40 @@
 window.APP_SETTINGS = {
-  applicationsOpen: true, // false = closes apply button
+  applicationsOpen: true,
 
   teams: [
     {
       name: "Dream Team",
       leaders: ["N/A"],
       description:
-        "Welcomes guests and helps everyone feel comfortable.",
+        "Fosters connections by warmly welcoming guests and ensuring everyone feels valued.",
       status: "Open"
     },
     {
       name: "Worship Team",
       leaders: ["Lyca"],
       description:
-        "Leads worship through music during services.",
+        "Leads congregants in worship through music during services.",
       status: "Closed"
     },
     {
       name: "Production Team",
       leaders: ["Zyphic"],
       description:
-        "Handles lighting and technical production.",
+        "Manages in-game stage lighting and technical production.",
       status: "Closed"
     },
     {
       name: "Media Team",
       leaders: ["Joey"],
       description:
-        "Creates and manages media content.",
+        "Creates and manages media content for the ministry.",
       requirements: "Must have a PC to apply!",
       status: "Closed",
       subteams: [
         {
           name: "Streamer Team",
           description:
-            "Provides camera angles for livestreams.",
+            "Provides camera angles to the livestream director.",
           status: "Closed"
         }
       ]
@@ -43,7 +43,7 @@ window.APP_SETTINGS = {
       name: "Ministerial Team",
       leaders: ["Pastor Gabriel"],
       description:
-        "Delivers God’s Word and spiritual guidance.",
+        "Delivers God’s Word and provides spiritual guidance.",
       status: "Closed"
     }
   ]
